7e345699fe0b74819ae0939b76104b314eeae918
Yaroslav Udodov <yaroslav.udodov@akurateco.com>
2019-03-29 15:16:29 +0200

    nginx client + teems service

--- client/src/components/game/football-field/football-field.js
+++ client/src/components/game/football-field/football-field.js
@@ -54,11 +54,12 @@ class FootballField extends Component {
     const teemsOnField = [[], []];
     let row = [];
     let rowNum = 1;
+    console.log(this.state.gameOrder);
     this.state.gameOrder.forEach((it, index) => {
       it.forEach((it)=> {
         row = [];
         let isActive = false;
-        for (let i = 1; i <= it; i++) {
+        for (let i = 1; i <= it && total > 0; i++) {
           isActive = (total - parseInt(game.active_players)) < 1;
           total--;
           row.push(
