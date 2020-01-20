b93f8a821b609b06f8e41a3335527244e67b1dcf
Yaroslav Udodov <yaroslav.udodov@akurateco.com>
2020-01-20 18:44:37 +0200

    gmail

--- api/app/middlewares/15-mailer.js
+++ api/app/middlewares/15-mailer.js
@@ -1,5 +1,6 @@
 import nodemailer from 'nodemailer'
 
+console.log(process.env.GMAIL_REFRESH_TOKEN, process.env.GMAIL_CLIENT_ID, process.env.GMAIL_CLIENT_SECRET);
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
