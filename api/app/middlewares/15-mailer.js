3c09a7d7e63b131cfa400bd1c361ddad6e918ae6
Yaroslav Udodov <yaroslav.udodov@akurateco.com>
2020-01-20 17:25:33 +0200

    remove env

--- api/app/middlewares/15-mailer.js
+++ api/app/middlewares/15-mailer.js
@@ -1,11 +1,16 @@
 import nodemailer from 'nodemailer'
+
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
+    type: 'OAuth2',
     user: 'f4econtactcenter@gmail.com',
-    pass: 'Stalker03',
+    refreshToken: '1//04F470xVAQdHTCgYIARAAGAQSNwF-L9IryVK09dXwwdrobhNXFhxP_ZHAqM9X9sV-joBfLV8eXfgYT4K7WPuZrIrm8hR20b90z6s',
+    clientId: '630779901626-hjskckj5bji0v483f4bso9vgh78tc2fi.apps.googleusercontent.com',
+    clientSecret: 'N-r_Sy2YIHzTYZOSai-gmqip',
   },
 });
+
 exports.init = (app) => app.use(async (ctx, next) => {
   ctx.mailer = transporter;
   await next();
