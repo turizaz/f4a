9c275a06fb46cb5d4f45fbbbd2fd336d324b8314
Yaroslav Udodov <yaroslav.udodov@akurateco.com>
2020-01-20 17:34:30 +0200

    gmail

--- api/app/middlewares/15-mailer.js
+++ api/app/middlewares/15-mailer.js
@@ -5,9 +5,9 @@ const transporter = nodemailer.createTransport({
   auth: {
     type: 'OAuth2',
     user: 'f4econtactcenter@gmail.com',
-    refreshToken: '1//04F470xVAQdHTCgYIARAAGAQSNwF-L9IryVK09dXwwdrobhNXFhxP_ZHAqM9X9sV-joBfLV8eXfgYT4K7WPuZrIrm8hR20b90z6s',
-    clientId: '630779901626-hjskckj5bji0v483f4bso9vgh78tc2fi.apps.googleusercontent.com',
-    clientSecret: 'N-r_Sy2YIHzTYZOSai-gmqip',
+    refreshToken: process.env.GMAIL_REFRESH_TOKEN,
+    clientId: process.env.GMAIL_CLIENT_ID,
+    clientSecret: process.env.GMAIL_CLIENT_SECRET,
   },
 });
 
