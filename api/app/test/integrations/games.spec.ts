da49369cf0f5aa870081e7ac8ace2c5344c50512
Yaroslav Udodov <yaroslav.udodov@akurateco.com>
2020-03-01 11:01:05 +0200

    debug

--- api/app/test/integrations/games.spec.ts
+++ api/app/test/integrations/games.spec.ts
@@ -11,21 +11,23 @@ chai.use(chaiHttp)
 const sinon = require('sinon')
 
 describe('routes : games', () => {
-  before(() => {
-    return knex.migrate
-        .rollback()
-        .then(() => {
-          return knex.migrate.latest();
-        })
-        .then(() => {
-          return knex.seed.run();
-        });
-  });
-  after(() => {
-    return knex.migrate.rollback();
-  })
+    beforeEach(() => {
+        return knex.migrate
+            .rollback()
+            .then(() => {
+              return knex.migrate.latest();
+            })
+            .then(() => {
+              return knex.seed.run();
+            })
+    })
 
-  it('should not add game if not logged', async () => {
+    afterEach(() => {
+        sinon.restore()
+        return knex.migrate.rollback();
+    })
+
+    it('should not add game if not logged', async () => {
         const res = await chai
             .request(app)
             .post('/game')
@@ -38,103 +40,85 @@ describe('routes : games', () => {
                 players: 5,
             })
         assert.strictEqual(res.status, 403)
-  })
+    })
 
-it('should add game if logged', async () => {
+    it('should add game if logged', async () => {
 
-    sinon.stub(auth, 'sendConfirmationEmail').callsFake(function fakeFn() {return true})
+        sinon.stub(auth, 'sendConfirmationEmail').callsFake(function fakeFn() {return true})
 
-    await chai
+        await chai
+            .request(app)
+            .post('/auth/registration')
+            .send(MockedUser)
+
+        await chai
+            .request(app)
+            .get(`/auth/confirm-email/${encrypt(MockedUser.email)}`)
+            .set('content-type', 'application/json')
+
+        const res = await chai
+            .request(app)
+            .post(`/auth/login`)
+            .set('content-type', 'application/json')
+            .send(MockedUserCredetials)
+
+        const token = res.headers['set-cookie'][0]
+        const refreshToken = res.headers['set-cookie'][1]
+
+        const addGameRes = await chai
+            .request(app)
+            .post('/game')
+            .set('Cookie', token+';'+refreshToken)
+            .send({
+                additional: '',
+                address: 'dummy address',
+                city: 'Kiev',
+                city_id: 1,
+                date: '2019-02-09T22:22',
+                players: 5,
+            })
+        assert.strictEqual(addGameRes.status, 201)
+    })
+
+  it('should load game', async () => {
+      sinon.stub(auth, 'sendConfirmationEmail').callsFake(function fakeFn() {return true})
+
+      await chai
+          .request(app)
+          .post('/auth/registration')
+          .send(MockedUser)
+
+      await chai
+          .request(app)
+          .get(`/auth/confirm-email/${encrypt(MockedUser.email)}`)
+          .set('content-type', 'application/json')
+
+      const res = await chai
+          .request(app)
+          .post(`/auth/login`)
+          .set('content-type', 'application/json')
+          .send(MockedUserCredetials)
+
+      const token = res.headers['set-cookie'][0]
+      const refreshToken = res.headers['set-cookie'][1]
+
+      await chai
+          .request(app)
+          .post('/game')
+          .set('Cookie', token+';'+refreshToken)
+          .send({
+              additional: '',
+              address: 'dummy address',
+              city: 'Kiev',
+              city_id: 1,
+              date: '2019-02-09T22:22',
+              players: 5,
+          })
+
+    const resGet = await chai
         .request(app)
-        .post('/auth/registration')
-        .send(MockedUser)
+        .get('/game/1')
 
-    await chai
-        .request(app)
-        .get(`/auth/confirm-email/${encrypt(MockedUser.email)}`)
-        .set('content-type', 'application/json')
-
-    const res = await chai
-        .request(app)
-        .post(`/auth/login`)
-        .set('content-type', 'application/json')
-        .send(MockedUserCredetials)
-
-    const token = res.headers['set-cookie'][0]
-    const refreshToken = res.headers['set-cookie'][1]
-
-    const addGameRes = await chai
-        .request(app)
-        .post('/game')
-        .set('Cookie', token+';'+refreshToken)
-        .send({
-            additional: '',
-            address: 'dummy address',
-            city: 'Kiev',
-            city_id: 1,
-            date: '2019-02-09T22:22',
-            players: 5,
-        })
-    assert.strictEqual(addGameRes.status, 201)
-})
-
-  // it('should add game if logged', async () => {
-  //     await chai
-  //         .request(app)
-  //         .get(`/auth/confirm-email/${encrypt(MockedUser.email)}`)
-  //         .set('content-type', 'application/json')
-  //
-  //     await chai
-  //         .request(app)
-  //         .post(`/auth/login`)
-  //         .set('content-type', 'application/json')
-  //         .send(MockedUserCredetials)
-  //
-  //     const res = await chai
-  //         .request(app)
-  //         .post('/game')
-  //         .send({
-  //             additional: '',
-  //             address: 'dummy address',
-  //             city: 'Kiev',
-  //             city_id: 1,
-  //             date: '2019-02-09T22:22',
-  //             players: 5,
-  //         })
-  //     assert.strictEqual(res.status, 200)
-  // })
-
-  // it('should add new game', (done) => {
-  //   chai
-  //       .request(app)
-  //       .post('/game')
-  //       // .set(
-  //       //     'Authorization',
-  //       //     'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiZW1' +
-  //       //     'haWwiOiJ0ZXN0QGdtYWlsLmNvbSIsImlhdCI6MTU0OTUzNDk0N30.kQ8jgU' +
-  //       //     'cehChy0CYcoJCiOGIPubRGllrvDGeRgcfDCK4'
-  //       // )
-  //       .send({
-  //         additional: '',
-  //         address: 'Котляревського 65, 44',
-  //         city: 'Kiev',
-  //         city_id: 1,
-  //         date: '2019-02-09T22:22',
-  //         players: 5,
-  //       })
-  //       .end((err, res) => {
-  //         should.not.exist(err);
-  //         assert.strictEqual(res.status, 201);
-  //         done();
-  //       });
-  // });
-  // it('should load game', (done) => {
-  //   chai
-  //       .request(app)
-  //       .get('/game/1')
-  //       .end((err, res) => {
-  //         assert.strictEqual(res.body.status, 'forming');
-  //         done();
-  //       });
-  // });
+    assert.strictEqual(resGet.body.status, 'forming');
+  });
 });
